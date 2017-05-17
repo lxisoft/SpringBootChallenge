6cdaa98147b804b66f3bfc68be699454463e0134
shanees <shanees.m.k@lxisoft.com>
2017-05-17 10:17:41 +0530

    first itration EasyJob program

---
+++ shanees/src/main/java/com/lxisoft/easyjob/entity/User.java
@@ -0,0 +1,66 @@
+package com.lxisoft.easyjob.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id=1;
+	private String name;
+	private String email;
+	private String password;
+	private String rePassword;
+	@Lob
+	private byte[] image;
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRePassword() {
+		return rePassword;
+	}
+	public void setRePassword(String rePassword) {
+		this.rePassword = rePassword;
+	}
+	public byte[] getImage() {
+		return image;
+	}
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+	
+	
+	
+	
+	
+
+}
