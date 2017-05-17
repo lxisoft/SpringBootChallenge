fd5b2475a004d9d22c6842b47981a7dbfff37d07
arun <arunjohnson123@gmail.com>
2017-05-17 10:06:26 +0530

    HR app test-code

---
+++ arun_johnson/src/main/java/com/lxisoft/HrSchemaApplication.java
@@ -0,0 +1,12 @@
+package com.lxisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrSchemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrSchemaApplication.class, args);
+	}
+}
