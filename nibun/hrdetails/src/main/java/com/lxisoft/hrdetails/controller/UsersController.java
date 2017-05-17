b7d7b794895a3fc77bb734a3dfdc24ef87390494
nibun <nibun.p@gmail.com>
2017-05-17 16:11:46 +0530

    third commit

--- nibun/hrdetails/src/main/java/com/lxisoft/hrdetails/controller/UsersController.java
+++ nibun/hrdetails/src/main/java/com/lxisoft/hrdetails/controller/UsersController.java
@@ -1,6 +1,9 @@
 package com.lxisoft.hrdetails.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +34,20 @@ public class UsersController {
 	@RequestMapping("/saving")
 	public String saveUser(@ModelAttribute User user) {
 		usersRepo.save(user);
-
+		
+		
 		return "Registration successfully completed";
+	}
+	
+
+	
+	
+	@RequestMapping("/getusers")
+	public ModelMap listOfUsers(ModelMap map){
+		ModelMap userMap= new ModelMap("listofuser");
+		List<User> list= usersRepo.findAll();
+		map.put("list", list);
+		return  userMap;
+		
 	}
 }
--- nibun/hrdetails/src/main/webapp/listofusers.jsp
+++ nibun/hrdetails/src/main/webapp/listofusers.jsp
@@ -7,17 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-<form action="/saving">
-	<center>
-		NAME:<input type= "text" name="name"><br>
-		ADDRESS:<input type = "text" name="address"><br>
-		EMAIL-ID:<input type ="text" name ="emilId"><br>
-		PASSWORD:<input type = "password" name="password">
-		<input type="submit" name="submit" value = "SUBMIT">
-	
-	</center>
-
-</form>
-
+	${list}
 </body>
 </html>
