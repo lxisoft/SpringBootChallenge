7f20feadf1aec17ae68bd9573c9a5dea5d8618eb
vidyasnair <vidya.s@lxisoft.com>
2017-05-18 13:30:59 +0530

    version5

---
+++ vidya/version5/src/main/java/com/lxisoft/UserProfile/UserProfileSoftware.java
@@ -0,0 +1,16 @@
+package com.lxisoft.UserProfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class UserProfileSoftware {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			SpringApplication.run(UserProfileSoftware.class, args);
+	}
+
+}
